5d7b8395782ad2bd3885ee1a4edcd37a4879cd3d
devkage <tillayevx1@gmail.com>
2025-02-18 20:14:27 +0500

    Added Profile and sidebar

--- client/app/(chat)/_components/chat.tsx
+++ client/app/(chat)/_components/chat.tsx
@@ -5,20 +5,39 @@ import { Form, FormControl, FormField, FormItem } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { messageSchema } from '@/lib/validation'
 import { Paperclip, Send, Smile } from 'lucide-react'
-import React, { FC } from 'react'
+import React, { FC, useRef } from 'react'
 import { UseFormReturn } from 'react-hook-form'
 import { z } from 'zod'
 import emojies from '@emoji-mart/data'
 import Picker from '@emoji-mart/react'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { useTheme } from 'next-themes'
 
 interface Props {
   onSendMessage: (values: z.infer<typeof messageSchema>) => void
   messageForm: UseFormReturn<z.infer<typeof messageSchema>>
 }
   
-
 const Chat: FC<Props> = ({onSendMessage, messageForm}) => {
+  
+  const {resolvedTheme} = useTheme()
+  const inputRef = useRef<HTMLInputElement | null>(null)
+  const handleEmojiSelect = (emoji: string) => {
+    const input = inputRef.current
+    if (!input) return
+
+    const text = messageForm.getValues('text')
+    const start = input.selectionStart ?? 0
+    const end = input.selectionEnd ?? 0
+
+    const newText = text.slice(0, start) + emoji + text.slice(end)
+    messageForm.setValue('text', newText)
+
+    setTimeout(() => {
+      input.setSelectionRange(start + emoji.length, start + emoji.length)
+    }, 0)
+  }
+
   return (
     <div className='flex flex-col justify-end z-40 min-h-[92vh]'>
       {/* Loading */}
@@ -50,6 +69,7 @@ const Chat: FC<Props> = ({onSendMessage, messageForm}) => {
                     value={field.value}
                     onBlur={() => field.onBlur()}
                     onChange={e => field.onChange(e.target.value)}
+                    ref={inputRef}
                   />
                 </FormControl>
               </FormItem>
@@ -62,7 +82,9 @@ const Chat: FC<Props> = ({onSendMessage, messageForm}) => {
                   <Smile/>
                 </Button>
               </PopoverTrigger>
-              <PopoverContent>Place content for the popover here.</PopoverContent>
+              <PopoverContent className='p-0 border-none rounded-md absolute right-6 bottom-0'>
+                <Picker data={emojies} theme={resolvedTheme === 'dark' ? 'dark' : 'light'} onEmojiSelect={(emoji:{native: string}) => handleEmojiSelect(emoji.native)}/>               
+              </PopoverContent>
             </Popover>
 
             <Button type='submit' size={'icon'}>
--- client/app/(chat)/_components/settings.tsx
+++ client/app/(chat)/_components/settings.tsx
@@ -1,12 +1,83 @@
 import { Button } from '@/components/ui/button'
-import { Menu } from 'lucide-react'
-import React from 'react'
+import { LogIn, Menu, Moon, Settings2, Sun, UserPlus, VolumeOff } from 'lucide-react'
+import { Popover,PopoverContent,PopoverTrigger} from "@/components/ui/popover"
+import { Separator } from '@/components/ui/separator'
+import { Switch } from '@/components/ui/switch'
+import { useTheme } from 'next-themes'
+import {Sheet,SheetContent,SheetDescription,SheetHeader,SheetTitle} from "@/components/ui/sheet"
+import { useState } from 'react'
+
 
 const Settings = () => {
+
+  const [ isProfileOpen, setIsProfileOpen] = useState(false)
+  const { resolvedTheme, setTheme } = useTheme()
+ 
   return (
-    <Button size={'icon'} variant={'secondary'}>
-        <Menu />
-    </Button>
+    <>
+      <Popover>
+        <PopoverTrigger>        
+          <Button size={'icon'} variant={'secondary'} >
+              <Menu />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className='p-0 w-80'>
+          <h2 className='pt-2 pl-2 text-muted-foreground'>
+            Settings: <span className='text-white'>tillayevx1@gmail.com</span>
+          </h2>
+          <Separator className='my-2'/>
+          <div className='flex flex-col'>
+            <div className='flex justify-between items-center p-2 hover:bg-secondary cursor-pointer' onClick={() => setIsProfileOpen(true)}>
+              <div className='flex items-center gap-1'>
+                <Settings2 size={16}/> 
+                {/* <SlidersIcon />< */}
+                <span className='text-sm'>Profile</span>
+              </div>
+            </div>
+            <div className='flex justify-between items-center p-2 hover:bg-secondary cursor-pointer'>
+            <div className='flex items-center gap-1' onClick={() => setIsProfileOpen(true)}>
+                <UserPlus size={16}/> 
+                <span className='text-sm'>Add contact</span>
+              </div>
+            </div>
+            <div className='flex justify-between items-center p-2 hover:bg-secondary'>
+              <div className='flex items-center gap-1'>
+                <VolumeOff size={16}/> 
+                <span className='text-sm'>Mute</span>
+              </div>
+              <Switch />
+            </div>
+            <div className='flex justify-between items-center p-2 hover:bg-secondary'>
+              <div className='flex items-center gap-1'>
+                {resolvedTheme === 'dark' ? <Sun size={16}/> : <Moon size={16}/> }
+                <span className='text-sm'>{resolvedTheme === 'dark' ? 'Light mode' : 'Dark mode'}</span>
+              </div>
+              <Switch 
+                checked={resolvedTheme === 'dark' ? true: false}
+                onCheckedChange={() => setTheme(resolvedTheme === 'dark' ? 'light':'dark')}
+              />
+            </div>
+            <div className='flex justify-between items-center bg-destructive p-2 cursor-pointer'>
+              <div className='flex items-center gap-1'>
+                <LogIn size={16}/> 
+                <span className='text-sm'>Logout</span>
+              </div>
+            </div>
+          </div>
+        </PopoverContent>
+      </Popover>
+
+      <Sheet open={isProfileOpen} onOpenChange={setIsProfileOpen}>
+        <SheetContent side={'left'} className='w-80 p-2'> 
+          <SheetHeader>
+            <SheetTitle className='text-2xl'>Profile</SheetTitle>
+            <SheetDescription>
+              Setting up your profile and level up
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent> 
+      </Sheet>
+    </>
   )
 }
 
