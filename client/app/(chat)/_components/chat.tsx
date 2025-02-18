b461947cd8e53b0418498004b101b52d72e403dc
devkage <tillayevx1@gmail.com>
2025-02-18 15:42:52 +0500

    Added chat and Chat configurations

--- client/app/(chat)/_components/chat.tsx
+++ client/app/(chat)/_components/chat.tsx
@@ -1,15 +1,75 @@
 // import ChatLoading from '@/components/loadings/chat.loading'
 // import MessageCard from '@/components/cards/message.card'
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { messageSchema } from '@/lib/validation'
+import { Paperclip, Send, Smile } from 'lucide-react'
+import React, { FC } from 'react'
+import { UseFormReturn } from 'react-hook-form'
+import { z } from 'zod'
+import emojies from '@emoji-mart/data'
+import Picker from '@emoji-mart/react'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 
-const Chat = () => {
+interface Props {
+  onSendMessage: (values: z.infer<typeof messageSchema>) => void
+  messageForm: UseFormReturn<z.infer<typeof messageSchema>>
+}
+  
+
+const Chat: FC<Props> = ({onSendMessage, messageForm}) => {
   return (
     <div className='flex flex-col justify-end z-40 min-h-[92vh]'>
       {/* Loading */}
       {/* <ChatLoading /> */}
+
       {/* Messages */}
-      {/* <MessageCard /> */}
+      {/* <MessageCard isReceived /> */}
+      
+      {/* Start kaiwa */}
+      <div className='w-full h-[88vh] flex items-center justify-center'>
+        <div className='text-[100px] cursor-pointer' onClick={() => onSendMessage({text: '✍️'})}>✍️</div>
+      </div>
+      
       {/*  Message Input*/}
+      <Form {...messageForm}>
+        <form onSubmit={messageForm.handleSubmit(onSendMessage)} className='w-full flex relative'>
+          <Button size={'icon'} type='button' variant={'secondary'}>
+              <Paperclip />
+          </Button>
+          <FormField
+            control={messageForm.control}
+            name="text"
+            render={({ field }) => (
+              <FormItem className='w-full'>
+                <FormControl>
+                  <Input 
+                    className='bg-secondary border-1 border-l-muted-foreground border-r border-r-muted-foreground h-9'
+                    placeholder='Type a message'
+                    value={field.value}
+                    onBlur={() => field.onBlur()}
+                    onChange={e => field.onChange(e.target.value)}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+            />
+
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button size={'icon'} type='button' variant={'secondary'}>
+                  <Smile/>
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent>Place content for the popover here.</PopoverContent>
+            </Popover>
+
+            <Button type='submit' size={'icon'}>
+              <Send/>
+            </Button>
+        </form>
+      </Form>
     </div>
   )
 }
--- client/app/(chat)/page.tsx
+++ client/app/(chat)/page.tsx
@@ -7,7 +7,7 @@ import AddContact from './_components/add-contact'
 import { useCurrentContact } from '@/hooks/use-current'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
-import { emailSchema } from '@/lib/validation'
+import { emailSchema, messageSchema } from '@/lib/validation'
 import { zodResolver } from '@hookform/resolvers/zod'
 import TopChat from './_components/top-chat'
 import Chat from './_components/chat'
@@ -22,11 +22,22 @@ const Homepage = () => {
       defaultValues: { email: ""},
   })
 
+  const messageForm = useForm<z.infer<typeof messageSchema >>({
+    resolver: zodResolver(messageSchema),
+    defaultValues: { text: '', image: ''},
+  })
+
   useEffect(() => {
     router.replace('/')
   }, [])
 
   const onCreateContact = (values: z.infer<typeof emailSchema>) => {
+    // API call
+    console.log(values)
+  }
+
+  const onSendMessage = (values: z.infer<typeof messageSchema>) => {
+    // API call
     console.log(values)
   }
 
@@ -51,7 +62,7 @@ const Homepage = () => {
           {/* Top chat */}
           <TopChat />
           {/* Chat message */}
-          <Chat />
+          <Chat  messageForm={messageForm} onSendMessage={onSendMessage} />
         </div> }
       </div>
   </>
@@ -60,11 +71,12 @@ const Homepage = () => {
 }
 
 const contacts = [
-  {email: 'john@gmail.com', _id: '1', avatar: 'https://github.com/shadcn.png'},
+  {email: 'john@gmail.com', _id: '1', avatar: 'https://github.com/shadcn.png', firstName: "Johan", lastName: "Yonju", bio: "My hobbi is development"},
   {email: 'milly@gmail.com', _id: '2', avatar: 'https://github.com/shadcn.png'},
   {email: 'trit@gmail.com', _id: '3', avatar: 'https://github.com/shadcn.png'},
   {email: 'phoenix@gmail.com', _id: '4', avatar: 'https://github.com/shadcn.png'},
   {email: 'jack@gmail.com', _id: '5', avatar: 'https://github.com/shadcn.png'},
 ]
+
 
 export default Homepage
