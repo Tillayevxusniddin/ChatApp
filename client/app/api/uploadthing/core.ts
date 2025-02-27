85187df02dfd075add667916b67a4e3fd729125b
devkage <tillayevx1@gmail.com>
2025-02-27 16:04:37 +0500

    改善しました

--- client/app/api/uploadthing/core.ts
+++ client/app/api/uploadthing/core.ts
@@ -21,6 +21,32 @@
 
 // export type ourFileRouter = typeof ourFileRouter
 
+
+
+// import { authOptions } from "@/lib/auth-options";
+// import { getServerSession } from "next-auth";
+// import { createUploadthing, type FileRouter } from "uploadthing/next";
+// import { UploadThingError } from "uploadthing/server";
+
+// const f = createUploadthing();
+
+// export const ourFileRouter = {
+//     imageUploader: f({ image: { maxFileSize: "4MB" } })
+//         .middleware(async () => {
+//             const token = await getServerSession(authOptions);
+//             if (!token) {
+//                 throw new UploadThingError("UnAuthorized"); 
+//             }
+//             return { token };
+//         })
+//         // @ts-expect-error
+//         .onUploadComplete(async ({ file }) => {
+//             return file.ufsUrl; 
+//         }),
+// } satisfies FileRouter;
+
+// export type OurFileRouterType = typeof ourFileRouter;
+
 import { authOptions } from "@/lib/auth-options";
 import { getServerSession } from "next-auth";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
@@ -31,14 +57,14 @@ const f = createUploadthing();
 export const ourFileRouter = {
     imageUploader: f({ image: { maxFileSize: "4MB" } })
         .middleware(async () => {
-            const token = await getServerSession(authOptions);
-            if (!token) {
-                throw new UploadThingError("UnAuthorized"); 
+            const session = await getServerSession(authOptions);
+            if (!session) {
+                throw new UploadThingError("Unauthorized"); 
             }
-            return { token };
+            return { user: session.user }; 
         })
-        .onUploadComplete(async ({ metadata, file }) => {
-            return { file, metadata }; 
+        .onUploadComplete(async ({ file }) => {
+            return { fileUrl: file.ufsUrl }; 
         }),
 } satisfies FileRouter;
 
